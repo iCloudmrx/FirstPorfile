547eb15576153c0236dffb74542a1faad1bc03d4
Nurillo Mahmudjonov <72136850+iCloudmrx@users.noreply.github.com>
2023-04-22 10:06:35 +0500

    add hosts

--- mysite/settings.py
+++ mysite/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = 'django-insecure-*(9@j88^o@x=)&ph91!f6j)zrkj$a20e1j!7q%zgldtn!ovo4@
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['.pythonanywhere.com', '127.0.0.1']
 
 
 # Application definition
