24017ec08eee3527705ece1aa1f68018d87b802c
Nurillo Mahmudjonov <72136850+iCloudmrx@users.noreply.github.com>
2023-04-22 10:13:29 +0500

    update setting

--- mysite/settings.py
+++ mysite/settings.py
@@ -24,7 +24,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-*(9@j88^o@x=)&ph91!f6j)zrkj$a20e1j!7q%zgldtn!ovo4@'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['.pythonanywhere.com', '127.0.0.1']
 
