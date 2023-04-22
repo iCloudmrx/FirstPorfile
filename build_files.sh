aef20094653abffb616a6bd639b808e449d5d2c6
Nurillo Mahmudjonov <72136850+iCloudmrx@users.noreply.github.com>
2023-04-22 11:04:35 +0500

    update vercel

--- build_files.sh
+++ build_files.sh
@@ -1,4 +1,4 @@
 echo " BUILD START"
-python3.11.2 -m pip install -r requirements.txt
-python3.11.2 manage.py collectstatic --noinput --clear
+python3.10 -m pip install -r requirements.txt
+python3.10 manage.py collectstatic --noinput --clear
 echo " BUILD END"
